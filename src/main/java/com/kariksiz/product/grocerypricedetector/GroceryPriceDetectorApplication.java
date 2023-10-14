b11c58bcfab09de705a07c06d89531fb92ab9f13
yakup <yakupkariksiz@gmail.com>
2023-10-14 12:49:36 +0200

    feat(logback): integrate logback.

--- src/main/java/com/kariksiz/product/grocerypricedetector/GroceryPriceDetectorApplication.java
+++ src/main/java/com/kariksiz/product/grocerypricedetector/GroceryPriceDetectorApplication.java
@@ -1,13 +1,19 @@
 package com.kariksiz.product.grocerypricedetector;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GroceryPriceDetectorApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(GroceryPriceDetectorApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("GroceryPriceDetector application has been started!");
 		SpringApplication.run(GroceryPriceDetectorApplication.class, args);
+		logger.info("Already started");
 	}
 
 }
